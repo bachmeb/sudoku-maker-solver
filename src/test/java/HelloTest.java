46284e043ed1215afaaee873d5a604050add2626
brian <brian@pop-os.localdomain>
2021-01-30 17:35:18 -0500

    add test and logging

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class HelloTest {
+
+    static final Logger logger =
+            LoggerFactory.getLogger(HelloTest.class);
+
+    Hello fixture;
+
+    @BeforeEach
+    void setUp() {
+
+        logger.info("setting up");
+        fixture = new Hello();
+
+
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        fixture = null;
+    }
+
+    @Test
+    void main() {
+        fixture.main(null);
+    }
+}
