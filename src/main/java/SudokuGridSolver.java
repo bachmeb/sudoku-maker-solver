d102f9506f50f3359e719ed591547eb22d73b6a4
bachmeb <bachmeb@mail.uc.edu>
2021-01-30 23:24:06 -0500

    add algorithm notes

--- src/main/java/SudokuGridSolver.java
+++ src/main/java/SudokuGridSolver.java
@@ -2,7 +2,43 @@ public class SudokuGridSolver {
 
     public SudokuGrid solve(SudokuGrid grid){
 
+        // is the grid solved?
+
+        // count the number of each number in each box
+
+        // count the number of each number in each row
+
+        // count the number of each number in each column
+
+        // count the number of numbers in each box
+
+        // count the number of numbers in each row
+
+        // count the number of numbers in each column
+
+        // if the number of numbers in a given box is 8 then add the last number
+
+        // if the number of numbers in a given row is 8 then add the last number
+
+        // if the number of numbers in a given column is 8 then add the last number
+
+        // start with the number found in the most boxes
+
+        // go to each box without that number
+
+        // go to each empty square in that box
+
+        // see if that number is in any other square in the same row
+
+        // see if that number is in any other square in the same column
+
+        // determine if that number can be added to any other square in the same box
+
+        // if a number can be added to an empty square and cannot be added to any other square in the same box, row, or square
+        // then add that number to the empty square
+        
         return grid;
+
     }
 
 }
