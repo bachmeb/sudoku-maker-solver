83b72930a6c1a89862b2b81ad7510eb7c11d7afe
bachmeb <bachmeb@mail.uc.edu>
2021-02-15 23:11:16 -0500

    there's a bug in solveByEliminationInAdjacentBoxesHorizontal

--- src/main/java/SudokuGridSolver.java
+++ src/main/java/SudokuGridSolver.java
@@ -23,13 +23,6 @@ public class SudokuGridSolver {
 
         SudokuGridChecker checker = new SudokuGridChecker();
 
-        // does the grid have any errors?
-        boolean hasError = checker.checkGridForErrors(grid);
-
-        if (hasError) {
-            logger.info("this grid has more than one of the same number in a box, row, or column");
-            return grid;
-        }
 
         // is the grid solved?
         solverLoop:
@@ -43,34 +36,46 @@ public class SudokuGridSolver {
 
             logger.info("loop number: " + loop++ + " - not yet solved");
 
+            // does the grid have any errors?
+            boolean hasError = checker.checkGridForErrors(grid);
+
+            if (hasError) {
+                logger.info("this grid has more than one of the same number in a box, row, or column");
+                return grid;
+            }
+
             reCountNumbersInSquares();
 
             boolean gotOne;
 
             gotOne = solveSetsOfEight();
-            if(gotOne){
+            if (gotOne) {
                 logger.info("got one");
                 continue;
             }
 
             gotOne = solveByCrossChecking();
-            if(gotOne){
+            if (gotOne) {
                 logger.info("got one");
                 continue;
             }
 
-            gotOne = solveByEliminationInBoxesAdjacentToTwoCompletedBoxes();
-            if(gotOne){
+            gotOne = solveByEliminationInAdjacentBoxesHorizontal();
+            if (gotOne) {
                 logger.info("got one");
                 continue;
             }
 
-            // quit after x number of loops so as not to go on infinitely
-            if (loop == 100) {
-                logger.info("I give up after " + loop + " loops!");
-                logger.info(grid.toString());
-                return grid;
+            gotOne = solveByEliminationInAdjacentBoxesVertical();
+            if (gotOne) {
+                logger.info("got one");
+                continue;
             }
+
+            // quit if we get this far because every method has been tried
+            logger.info("I give up after " + loop + " loops! I've tried everything I know how to do...");
+            logger.info(grid.toString());
+            return grid;
 
         }
 
@@ -78,13 +83,12 @@ public class SudokuGridSolver {
             logger.info("This grid is solved after " + loop + " loops!!!");
         }
         logger.info(grid.toString());
-
         return grid;
 
     }
 
-    private boolean solveByEliminationInBoxesAdjacentToTwoCompletedBoxes() {
-        logger.info("solve by elimination in adjacent boxes and rows");
+    private boolean solveByEliminationInAdjacentBoxesHorizontal() {
+        logger.info("solve by elimination in adjacent boxes and rows horizontal");
 
         // get a list of numbers 1 - 9
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -102,32 +106,32 @@ public class SudokuGridSolver {
                 if (!numberInMindIsInBox) {
 
                     // get the two boxes horizontally adjacent to the box
-                    int[] otherTwoHorizontal = findTwoHorizontallyAdjacentSquaresInBox(boxIndex);
+                    int[] otherTwoAdjacentBoxesHorizontal = findTwoHorizontallyAdjacentSquaresInBox(boxIndex);
 
                     // see if the number is in the other two horizontally adjacent boxes
                     int count = 0;
-                    for (int hBoxIndex : otherTwoHorizontal) {
+                    for (int hBoxIndex : otherTwoAdjacentBoxesHorizontal) {
                         boolean itsThere = checkSetForNumber(numberInMind, grid.getBoxes()[hBoxIndex]);
                         if (itsThere) {
                             count++;
                         }
                     }
 
-                    // if yes then get the index number of the row that doesn't have the number in mind
+                    // if yes then get the index numbers of the two rows that have the number in mind
                     if (count == 2) {
-                        int otherRowNums[] = new int[2];
+                        int otherRowsThatHaveTheNumberInMind[] = new int[2];
                         int otherRowsIndex = 0;
-                        for (int i = 0; i < otherTwoHorizontal.length; i++) {
-                            int[] box = grid.getBoxes()[otherTwoHorizontal[i]];
+                        for (int i = 0; i < otherTwoAdjacentBoxesHorizontal.length; i++) {
+                            int[] box = grid.getBoxes()[otherTwoAdjacentBoxesHorizontal[i]];
                             for (int pos = 0; pos < 9; pos++) {
                                 if (box[pos] == numberInMind) {
-                                    int rowNum = grid.findRowNumForBoxNumAndPosNum(otherTwoHorizontal[i], pos);
-                                    otherRowNums[otherRowsIndex++] = rowNum;
+                                    int rowNum = grid.findRowNumForBoxNumAndPosNum(otherTwoAdjacentBoxesHorizontal[i], pos);
+                                    otherRowsThatHaveTheNumberInMind[otherRowsIndex++] = rowNum;
                                 }
                             }
                         }
 
-                        // select the three squares in the row of the box that isn't in the same row as the two that already have the number
+                        // select the three squares that aren't in the same row as the two that already have the number
                         int candidateSquares[] = new int[3];
                         int candidateSquaresPos[] = new int[3];
                         int candidateIndex = 0;
@@ -135,11 +139,14 @@ public class SudokuGridSolver {
                             int rowNum = grid.findRowNumForBoxNumAndPosNum(boxIndex, posNum);
                             boolean inOther = false;
                             for (int i = 0; i < 2; i++) {
-                                if (rowNum == otherRowNums[i]) {
+                                if (rowNum == otherRowsThatHaveTheNumberInMind[i]) {
                                     inOther = true;
                                 }
                             }
                             if (!inOther) {
+                                if(candidateIndex>2){
+                                    throw  new RuntimeException("What's happening???");
+                                }
                                 candidateSquares[candidateIndex] = grid.getBoxes()[boxIndex][posNum];
                                 candidateSquaresPos[candidateIndex++] = posNum;
                             }
@@ -166,12 +173,141 @@ public class SudokuGridSolver {
                                 }
                             }
                         }
+
+                        // if the count is one see if one of the available squares already has the number in mind in the same column
+                        if (countOfOtherOccupied == 1) {
+                            int firstSquarePos = -1;
+                            boolean foundNumberInMindInFirstColumn = false;
+                            for (int b = 0; b < 3; b++) {
+                                if (candidateSquares[b] == 0) {
+                                    if (foundNumberInMindInFirstColumn) {
+                                        int box[] = grid.getBoxes()[boxIndex];
+                                        box[candidateSquaresPos[b]] = numberInMind;
+                                        int[][] boxes = grid.getBoxes();
+                                        boxes[boxIndex] = box;
+                                        grid.setBoxes(boxes);
+                                        return true;
+                                    } else if (firstSquarePos == -1) {
+                                        firstSquarePos = candidateSquaresPos[b];
+                                    }
+                                    // get the column of numbers for that square
+                                    int colNum = grid.findColNumForBoxNumAndPosNum(boxIndex, candidateSquaresPos[b]);
+                                    int[] column = grid.getColumns()[colNum];
+                                    logger.debug(intArrayToString(column));
+
+                                    // see if that number is in any other square in that column
+                                    for (int c = 0; c < column.length; c++) {
+                                        if (column[c] == numberInMind) {
+                                            if (foundNumberInMindInFirstColumn == false && firstSquarePos == -1) {
+                                                foundNumberInMindInFirstColumn = true;
+                                            } else if (foundNumberInMindInFirstColumn == false && firstSquarePos > -1) {
+                                                int box[] = grid.getBoxes()[boxIndex];
+                                                box[candidateSquaresPos[b]] = numberInMind;
+                                                int[][] boxes = grid.getBoxes();
+                                                boxes[boxIndex] = box;
+                                                grid.setBoxes(boxes);
+                                                return true;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean solveByEliminationInAdjacentBoxesVertical() {
+        logger.info("solve by elimination in adjacent boxes and rows vertical");
+
+        // get a list of numbers 1 - 9
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        // loop through the list
+        for (int numberInMind : numbers) {
+
+            // loop through every box
+            for (int boxIndex = 0; boxIndex < grid.getBoxes().length; boxIndex++) {
+
+                // see if the number in mind is in the box
+                boolean numberInMindIsInBox = checkSetForNumber(numberInMind, grid.getBoxes()[boxIndex]);
+
+                // if the number in mind isn't in the box
+                if (!numberInMindIsInBox) {
 
                     // get the two boxes vertically adjacent to the box
-                    int[] otherTwoVertical = findTwoVerticallyAdjacentSquaresInABox(numberInMind);
+                    int[] adjacentBoxesVertical = findTwoVerticallyAdjacentSquaresInABox(boxIndex);
 
+                    // see if the number is in the other two adjacent boxes
+                    int count = 0;
+                    for (int vBoxIndex : adjacentBoxesVertical) {
+                        boolean itsThere = checkSetForNumber(numberInMind, grid.getBoxes()[vBoxIndex]);
+                        if (itsThere) {
+                            count++;
+                        }
+                    }
 
+                    // if yes then get the index numbers of the columns that have the number in mind
+                    if (count == 2) {
+                        int otherColNums[] = new int[2];
+                        int otherColsIndex = 0;
+                        for (int i = 0; i < adjacentBoxesVertical.length; i++) {
+                            int[] box = grid.getBoxes()[adjacentBoxesVertical[i]];
+                            for (int pos = 0; pos < 9; pos++) {
+                                if (box[pos] == numberInMind) {
+                                    int colNum = grid.findColNumForBoxNumAndPosNum(adjacentBoxesVertical[i], pos);
+                                    otherColNums[otherColsIndex++] = colNum;
+                                }
+                            }
+                        }
+
+                        // select the three squares in the column of the box that isn't in the same column as the two that already have the number
+                        int candidateSquares[] = new int[3];
+                        int candidateSquaresPos[] = new int[3];
+                        int candidateIndex = 0;
+                        for (int posNum = 0; posNum < 9; posNum++) {
+                            int colNum = grid.findColNumForBoxNumAndPosNum(boxIndex, posNum);
+                            boolean inOther = false;
+                            for (int i = 0; i < 2; i++) {
+                                if (colNum == otherColNums[i]) {
+                                    inOther = true;
+                                }
+                            }
+                            if (!inOther) {
+                                if(candidateIndex>2){
+                                    throw  new RuntimeException("What's happening???");
+                                }
+                                candidateSquares[candidateIndex] = grid.getBoxes()[boxIndex][posNum];
+                                candidateSquaresPos[candidateIndex++] = posNum;
+                                logger.debug("candidateSquaresPos[ candidateIndex ]: " + candidateIndex);
+                            }
+                        }
+
+                        // see if two of the boxes are occupied by other numbers
+                        int countOfOtherOccupied = 0;
+                        for (int y = 0; y < 3; y++) {
+                            if (candidateSquares[y] > 0) {
+                                countOfOtherOccupied++;
+                            }
+                        }
+
+                        // if so then add the number in mind to the empty box
+                        if (countOfOtherOccupied == 2) {
+                            for (int b = 0; b < 3; b++) {
+                                if (candidateSquares[b] == 0) {
+                                    int box[] = grid.getBoxes()[boxIndex];
+                                    box[candidateSquaresPos[b]] = numberInMind;
+                                    int[][] boxes = grid.getBoxes();
+                                    boxes[boxIndex] = box;
+                                    grid.setBoxes(boxes);
+                                    return true;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
